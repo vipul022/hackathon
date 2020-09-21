aa9d443524a29ba99533a33e5779fa16d86f7eea
drumovski <jason.stacy@gmail.com>
2020-09-21 14:36:20 +1000

    added days of the week

--- main.js
+++ main.js
@@ -6,10 +6,11 @@ const temp = document.getElementById("temp");
 const max = document.getElementById("max");
 const min = document.getElementById("min");
 const description = document.getElementById("description");
+const days = document.getElementById("days")
 // console.log(inputValue);
 
 let getWeather = () => {
-    axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?city=Raleigh,NC&key=${WEATHERBIT_KEY}`)
+    axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?city=${inputValue.value}&country=AU&key=${WEATHERBIT_KEY}`)
     .then(response => {
     console.log(response)
     console.log(response.data.data[0].temp)
@@ -18,6 +19,11 @@ let getWeather = () => {
     max.innerHTML = `Max: ${response.data.data[0].max_temp}`
     min.innerHTML = `Min: ${response.data.data[0].min_temp}`
     description.innerHTML = `Description: ${response.data.data[0].weather.description}`
+    for (let i=1;i<8;i++){
+        let li = document.createElement('li')
+        li.innerHTML = `${dayOfTheWeek(response.data.data[i].datetime)} max: ${response.data.data[i].max_temp}`
+        days.appendChild(li)
+    }
     })
     .catch(err => console.log(err));
     console.log("test")
@@ -25,3 +31,19 @@ let getWeather = () => {
 
 document.querySelector(".button").addEventListener("click", getWeather)
 
+var weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+const dayOfTheWeek = (date) => {
+    let d = new Date(date);
+    var n = weekday[d.getDay()];
+    return n
+}
+console.log(dayOfTheWeek("2020-09-21"))
+// datetime: "2020-09-20"
