9847bae0d8998ce609d73b6a8c3f309de3ead2ef
vipul022 <vipul.seth022@gmail.com>
2020-09-21 14:16:11 +1000

    modify country in api

--- main.js
+++ main.js
@@ -9,19 +9,21 @@ const description = document.getElementById("description");
 // console.log(inputValue);
 
 let getWeather = () => {
-    axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?city=Raleigh,NC&key=${WEATHERBIT_KEY}`)
-    .then(response => {
-    console.log(response)
-    console.log(response.data.data[0].temp)
-    city.innerHTML = `Details for: ${response.data.city_name}`
-    temp.innerHTML = `Current temperature: ${response.data.data[0].temp}`
-    max.innerHTML = `Max: ${response.data.data[0].max_temp}`
-    min.innerHTML = `Min: ${response.data.data[0].min_temp}`
-    description.innerHTML = `Description: ${response.data.data[0].weather.description}`
+  axios
+    .get(
+      `https://api.weatherbit.io/v2.0/forecast/daily?city=${inputValue.value}&country=AU&key=${WEATHERBIT_KEY}`
+    )
+    .then((response) => {
+      console.log(response);
+      console.log(response.data.data[0].temp);
+      city.innerHTML = `Details for: ${response.data.city_name}`;
+      temp.innerHTML = `Current temperature: ${response.data.data[0].temp}`;
+      max.innerHTML = `Max: ${response.data.data[0].max_temp}`;
+      min.innerHTML = `Min: ${response.data.data[0].min_temp}`;
+      description.innerHTML = `Description: ${response.data.data[0].weather.description}`;
     })
-    .catch(err => console.log(err));
-    console.log("test")
-}
+    .catch((err) => console.log(err));
+  console.log("test");
+};
 
-document.querySelector(".button").addEventListener("click", getWeather)
-
+document.querySelector(".button").addEventListener("click", getWeather);
