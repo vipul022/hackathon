8b6def1daf6e445701bba22afae91c76c64cbb4a
drumovski <jason.stacy@gmail.com>
2020-09-21 13:30:43 +1000

    add more details, basic CSS

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
 import { WEATHERBIT_KEY } from "./keys.js";
 const inputValue = document.querySelector(".inputValue");
 const outputData = document.querySelector(".output-data");
+const city = document.getElementById("city");
+const temp = document.getElementById("temp");
+const max = document.getElementById("max");
+const min = document.getElementById("min");
+const description = document.getElementById("description");
 // console.log(inputValue);
 
 let getWeather = () => {
-    axios.get(`https://api.weatherbit.io/v2.0/current?city=Raleigh,NC&key=${WEATHERBIT_KEY}`)
+    axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?city=Raleigh,NC&key=${WEATHERBIT_KEY}`)
     .then(response => {
     console.log(response)
     console.log(response.data.data[0].temp)
-    let p = document.createElement("p")
-    let temp = document.createTextNode(`Current temperature is: ${response.data.data[0].temp}`)
-    outputData.appendChild(p)
-    p.appendChild(temp)
+    city.innerHTML = `Details for: ${response.data.city_name}`
+    temp.innerHTML = `Current temperature: ${response.data.data[0].temp}`
+    max.innerHTML = `Max: ${response.data.data[0].max_temp}`
+    min.innerHTML = `Min: ${response.data.data[0].min_temp}`
+    description.innerHTML = `Description: ${response.data.data[0].weather.description}`
     })
     .catch(err => console.log(err));
     console.log("test")
